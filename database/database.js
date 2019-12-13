4bc97454d3c7b2bd878257fd191eb8cca37f040d
Rafael Trindade Cavasani <rafael.cavasani@hotmail.com>
2019-12-13 10:40:03 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://localhost/girias_de_tiozao';
+const uri = 'mongodb+srv://unipnosql:unipmongodb@cluster0-1jtkx.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
